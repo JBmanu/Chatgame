9614b08ed2420baf43d245ef0a2f997d0c320d83
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2021-08-21 11:04:54 +0200

    add send function for client

--- bin/Client/ChatGUI.py
+++ bin/Client/ChatGUI.py
@@ -43,38 +43,29 @@ class ChatApplication(tk.Tk):
         self.msg_entry = tk.Entry(self.bottom_label, bg = "#2C3E50", fg = Utils.TEXT_COLOR, font = Utils.FONT)
         self.msg_entry.place(relwidth = 0.74, relheight = 0.06, rely = 0.008, relx = 0.011)
         self.msg_entry.focus()
-        self.msg_entry.bind("<Return>", self._premere_enter)
         
         # pulsante invio messaggio
-        self.pulsante_invio = tk.Button(self.bottom_label, text = "Invia", font = Utils.FONT_BOLD, width = 20, bg = Utils.BG_GRAY,
-                                command = lambda: self._premere_enter(None))
+        self.pulsante_invio = tk.Button(self.bottom_label, text = "Invia", font = Utils.FONT_BOLD, width = 20, bg = Utils.BG_GRAY)
         self.pulsante_invio.place(relx = 0.77, rely = 0.008, relheight = 0.06, relwidth = 0.22)
+
         
         #pulsante a
-        self.pulsante_a = tk.Button(self.under_label, text = "a", font = Utils.FONT_BUTTON, width = 5, bg = Utils.BUTTON_COLOR,
-                            command = lambda: self._premere_Char("a"))
+        self.pulsante_a = tk.Button(self.under_label, text = "a", font = Utils.FONT_BUTTON, width = 5, bg = Utils.BUTTON_COLOR)
         self.pulsante_a.place(relx = 0.1, rely = 0.2, relheight = 0.7, relwidth = 0.22)
         
         
         #pulsante b
-        self.pulsante_b = tk.Button(self.under_label, text = "b", font = Utils.FONT_BUTTON, width = 5, bg = Utils.BUTTON_COLOR,
-                            command = lambda: self._premere_Char("b"))
+        self.pulsante_b = tk.Button(self.under_label, text = "b", font = Utils.FONT_BUTTON, width = 5, bg = Utils.BUTTON_COLOR)
         self.pulsante_b.place(relx = 0.39, rely = 0.2, relheight = 0.7, relwidth = 0.22)
         
         
         #pulsante c
-        self.pulsante_c = tk.Button(self.under_label, text = "c", font = Utils.FONT_BUTTON, width = 5, bg = Utils.BUTTON_COLOR,
-                            command = lambda: self._premere_Char("c"))
+        self.pulsante_c = tk.Button(self.under_label, text = "c", font = Utils.FONT_BUTTON, width = 5, bg = Utils.BUTTON_COLOR)
         self.pulsante_c.place(relx = 0.68, rely = 0.2, relheight = 0.7, relwidth = 0.22)
         
         
     def _premere_Char(self, char):
         self._inserisci_messaggio(char, "Tu")
-        
-    
-    def _premere_enter(self, event):
-        msg  =  self.msg_entry.get()
-        self._inserisci_messaggio(msg, "Tu")
         
         
     def _inserisci_messaggio(self, msg, sender):
@@ -82,7 +73,14 @@ class ChatApplication(tk.Tk):
             return
         
         self.msg_entry.delete(0, tk.END)
-        msg1  =  f"{sender}: {msg}\n\n"
+        msg1  =  f"{sender}: {msg}\n"
         self.text_widget.configure(cursor = "arrow", state = tk.NORMAL)
         self.text_widget.insert(tk.END, msg1)
+        self.text_widget.configure(state = tk.DISABLED)
+        self.msg_entry.delete(0, tk.END)
+
+
+    def insertMsgFromTextToChat(self, msg):
+        self.text_widget.configure(state = tk.NORMAL)
+        self.text_widget.insert(tk.END, msg)
         self.text_widget.configure(state = tk.DISABLED)
